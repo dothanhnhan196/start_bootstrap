a306b8e945314dea3bb8f0304174ec9c164a8e25
dnnhan <dothanhnhan196>
2021-04-14 22:48:22 +0700

    no resposive

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#header")
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+$('.navbar-nav .nav-link').click(function() {
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+})
